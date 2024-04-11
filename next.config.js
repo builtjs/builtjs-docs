a47e4170199a09a0982162fa6edfce2d935c301c
Richard Lovell <richardlovell@Richards-MacBook-Air.local>
2024-04-11 23:05:09 +1200

    Updated start

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
+  themeConfig: './theme.config.tsx',
+  pageDataCollectionTimeout: 120000, // Increase timeout to 120 seconds
 })
 
 module.exports = withNextra()
